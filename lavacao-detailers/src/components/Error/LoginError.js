4844c3ca66770eb9f62ac254ac4f403490a1b24a
calian-d <129885009+calian-d@users.noreply.github.com>
2023-09-18 22:10:43 -0300

    Alterações na validação de login do form

--- lavacao-detailers/src/components/Error/LoginError.js
+++ lavacao-detailers/src/components/Error/LoginError.js
@@ -1,14 +1,32 @@
+import React, { useState, useEffect } from "react";
 import styles from "./LoginError.module.css";
 import { ReactComponent as Close } from "../../assets/img/close-bold.svg";
 
 export function LoginError({ error, onClick }) {
-    return (
-        <div className={styles.containerErro}>            
-            <p>{error}</p>
-            <button
-                className={styles.btClose}
-                onClick={onClick}
-            ><Close className={styles.button} /></button>
-        </div>
-    );
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(!!error);
+  }, [error]);
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    setIsVisible(false); // Define isVisible como false para iniciar o fade-out
+    
+    // Aguarde o tempo da transição (500ms) antes de chamar onClick
+    setTimeout(() => {
+      onClick();
+    }, 500);
+  };
+
+  return (
+    <div
+      className={`${styles.containerErro} ${isVisible ? styles.show : styles.hide}`}
+    >
+      <p>{error}</p>
+      <button className={styles.btClose} onClick={handleButtonClick}>
+        <Close className={styles.button} />
+      </button>
+    </div>
+  );
 }
--- lavacao-detailers/src/screens/LoginPage.js
+++ lavacao-detailers/src/screens/LoginPage.js
@@ -1,65 +1,65 @@
-import styles from "./LoginPage.module.css";
-import { BtLogin } from "../components/Buttons/BtLogin";
-import { UserLogin } from "../components/InputText/UserLogin";
-import { UserPassword } from "../components/InputText/UserPassword";
-import { useState } from "react";
-import LogoDetailer from "../assets/icon/detailer-logo-1-removebg-preview.png"
-import { LoginError } from "../components/Error/LoginError";
+  import styles from "./LoginPage.module.css";
+  import { BtLogin } from "../components/Buttons/BtLogin";
+  import { UserLogin } from "../components/InputText/UserLogin";
+  import { UserPassword } from "../components/InputText/UserPassword";
+  import { useState } from "react";
+  import LogoDetailer from "../assets/icon/detailer-logo-1-removebg-preview.png"
+  import { LoginError } from "../components/Error/LoginError";
 
-export function LoginPage() {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  export function LoginPage() {
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-  const handleLoginClick = (e) => {
-    // Validação de login e senha
+    const handleLoginClick = (e) => {
+      // Validação de login e senha
 
-    console.log("Login: "+ login)
-    console.log("Senha: "+ password)
+      console.log("Login: "+ login)
+      console.log("Senha: "+ password)
 
-    if (login === "calian" && password === "123") {
-      // Lógica de autenticação bem-sucedida
-      setError(""); // Limpa qualquer erro anterior
-      // Redirecione ou execute a ação desejada aqui
-    } else {
-      e.preventDefault();
+      if (login === "calian" && password === "123") {
+        // Lógica de autenticação bem-sucedida
+        setError(""); // Limpa qualquer erro anterior
+        // Redirecione ou execute a ação desejada aqui
+      } else {
+        e.preventDefault();
 
-      setError("Login ou Senha Inválido!"); // Define uma mensagem de erro
-    }
-  };
-  
-  return (
-    <form className={ styles.containerPage }>
-      <div className={ styles.containerBg }>
-        <div className={ styles.containerImg}>
-          <img 
-            src={ LogoDetailer }
-            alt="Logo Detailer"
-            className={ styles.logoDetailer}
+        setError("Login ou Senha Inválido!"); // Define uma mensagem de erro
+      }
+    };
+    
+    return (
+      <form className={ styles.containerPage }>
+        <div className={ styles.containerBg }>
+          <div className={ styles.containerImg}>
+            <img 
+              src={ LogoDetailer }
+              alt="Logo Detailer"
+              className={ styles.logoDetailer}
+            />
+          </div>
+          <div className={ styles.containerLogin }>
+              <UserLogin 
+                className={ styles.inputLogin }
+                name="idLogin"
+                value={login}
+                onChange={(e) => setLogin(e.target.value)}
+              />
+              <UserPassword 
+                className={ styles.inputSenha }
+                name="idPassword"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+          </div>
+          <BtLogin 
+            className={ styles.btLogin }
+            onClick={(e) => handleLoginClick(e)}
           />
+          <div className={ styles.containerErro }>
+            {error && <LoginError error={error} onClick={(e) => setError("")}/>}
+          </div>
         </div>
-        <div className={ styles.containerLogin }>
-            <UserLogin 
-              className={ styles.inputLogin }
-              name="idLogin"
-              value={login}
-              onChange={(e) => setLogin(e.target.value)}
-            />
-            <UserPassword 
-              className={ styles.inputSenha }
-              name="idPassword"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-        </div>
-        <BtLogin 
-          className={ styles.btLogin }
-          onClick={(e) => handleLoginClick(e)}
-        />
-        <div className={ styles.containerErro }>
-          {error && <LoginError error={error} onClick={(e) => setError("")}/>}
-        </div>
-      </div>
-    </form>
-  );
-}
+      </form>
+    );
+  }
