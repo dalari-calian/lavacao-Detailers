ebc6d44a253a92995fb12cfbdcc83f631755c1a2
calian-d <129885009+calian-d@users.noreply.github.com>
2023-11-09 14:44:14 -0300

    Adiciona validação na tela de login
    Adiciona validação na tela de login para fazer a requisição para o backend

--- lavacao-detailers/src/screens/LoginPage.js
+++ lavacao-detailers/src/screens/LoginPage.js
@@ -14,12 +14,39 @@
 
     const navigate = useNavigate();
 
-    const handleLoginClick = (e) => {
+    const handleLoginClick = async (e) => {
       // Validação de login e senha
+      e.preventDefault();
 
-      console.log("Login: "+ login)
-      console.log("Senha: "+ password)
+      console.log(login)
+      console.log(password)
 
+      try {
+        const response = await fetch("http://localhost:3333/login/validate", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ login, password }),
+        });
+        
+        if (response.ok) {
+          const data = await response.json();
+          console.log("Login successful:", data);
+          setError(""); // Limpa qualquer erro anterior
+          // Redirecione ou execute a ação desejada aqui
+          navigate('/homepage');
+        } else {
+          const data = await response.json();
+          console.error("Login failed:", data.message);
+          setError("Login ou Senha Inválido!"); // Define uma mensagem de erro
+        }
+
+      } catch (error) {
+        console.error("Error during login:", error);
+        setError("Erro durante o login. Tente novamente mais tarde.");
+      }
+      /*
       if (login === "calian" && password === "123") {
         // Lógica de autenticação bem-sucedida
         setError(""); // Limpa qualquer erro anterior
@@ -30,6 +57,7 @@
 
         setError("Login ou Senha Inválido!"); // Define uma mensagem de erro
       }
+      */
     };
     
     return (
