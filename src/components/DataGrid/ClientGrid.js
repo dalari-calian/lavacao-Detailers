c62c624ad7768887486083961f52366bd1d2408a
calian-d <129885009+calian-d@users.noreply.github.com>
2023-11-16 17:46:35 -0300

    Implementa customizações no grid

--- src/components/DataGrid/ClientGrid.js
+++ src/components/DataGrid/ClientGrid.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './ClientGrid.module.css';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, ptBR } from '@mui/x-data-grid';
 
 export function ClientGrid({ items }) {
   const columns = [
     { 
       field: 'firstName',
       headerName: 'Nome',
-      width: 300,
-      align: 'center',
+      width: 250,
+      headerClassName: style.headerStyle,
     },
     { 
       field: 'lastName',
       headerName: 'Sobrenome',
-      width: 300,
-      align: 'center',
+      width: 250,
+      headerClassName: style.headerStyle,
     },
     { 
       field: 'cpf',
       headerName: 'CPF',
-      width: 280,
-      align: 'center',
+      width: 230,
+      headerClassName: style.headerStyle,
     },
     { 
       field: 'email',
       headerName: 'Email',
-      width: 350,
-      align: 'center',
+      width: 300,
+      headerClassName: style.headerStyle,
     },
     { 
       field: 'phone',
       headerName: 'Celular',
-      width: 280,
-      align: 'center',
+      width: 230,
+      headerClassName: style.headerStyle,
     },
   ];
 
@@ -46,18 +46,9 @@ export function ClientGrid({ items }) {
     phone: client.phone,
   }));
   
-  const localizedTexts = {
-    
-    toolbarFilters: 'Filtros',
-    toolbarFiltersLabel: 'Mostrar filtros',
-    toolbarFiltersTooltipHide: 'Esconder filtros',
-    toolbarFiltersTooltipShow: 'Mostrar filtros',
-    
-  };
-  
   return (
     <div className={style.gridContainer}>
-      <div style={{ height: 650, width: '85vw' }}>
+      <div style={{ height: 600, width: '85vw' }}>
         <DataGrid
           className={style.gridStyle}
           rows={rows}
@@ -65,15 +56,27 @@ export function ClientGrid({ items }) {
           pageSize={5}
           rowsPerPageOptions={[5, 10, 20]}
           pagination
-          localeText={localizedTexts}
+          localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
           disableRowSelectionOnClick={true}
-          sx={{
+          sx={
+            {
+            'borderRadius': '20px',
+            'backgroundColor': '#F4F4F4',
+            'boxShadow': '4px 4px 40px 2px #00000040',
             '.MuiDataGrid-columnSeparator': {
               display: 'none',
             },
-            '&.MuiDataGrid-root': {
-              border: 'none',
+            '& .MuiDataGrid-columnHeaders': {
+              backgroundColor: "#C8C8C8",
+              borderRadius: "20px 20px 0px 0px",
+              paddingLeft: "1rem"
             },
+            '.MuiDataGrid-row': {
+              paddingLeft: "1rem"
+            },
+            '.MuiDataGrid-selectedRowCount': {
+              backgroundColor: "red"
+            }
           }}
         />
       </div>
