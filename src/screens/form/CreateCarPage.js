a17873648d47e96f28b30f7b10d9c9ce9779a72a
calian-d <129885009+calian-d@users.noreply.github.com>
2023-11-20 17:51:38 -0300

    Adiciona novo componente
    Adiciona novo componente componente que faz a seleção entre placa de carro de mercosul ou não.

--- src/screens/form/CreateCarPage.js
+++ src/screens/form/CreateCarPage.js
@@ -5,9 +5,7 @@ import { SuccessMessage } from "../../components/Popup/SuccessMessage";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { styled } from '@mui/material/styles';
-import Switch, { SwitchProps } from '@mui/material/Switch';
+import { SwitchIOS } from "../../components/Check/SwitchIOS";
 
 export function CreateCarPage() {
     const [modelName, setModelName] = useState("");
@@ -84,69 +82,21 @@ export function CreateCarPage() {
         setError(false);
     }
 
-    const handleMercosulSwitchChange = (event) => {
+    const handleSwitchChange = (event) => {
         setIsMercosul(event.target.checked);
-    }
-
+        if (event.target.checked) {
+            console.log("Ativado")
+        } else {
+            console.log("Desativado")
+        }
+    };
+    
     useEffect(() => {
         return () => {
             setSuccessMessage(null);
             setSuccess(false)
         };
     }, []);
-
-
-    const IOSSwitch = styled((props: SwitchProps) => (
-        <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-    ))(({ theme }) => ({
-        width: 42,
-        height: 26,
-        padding: 0,
-        '& .MuiSwitch-switchBase': {
-            padding: 0,
-            margin: 2,
-            transitionDuration: '300ms',
-            '&.Mui-checked': {
-            transform: 'translateX(16px)',
-            color: '#fff',
-            '& + .MuiSwitch-track': {
-                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#ffb623',
-                opacity: 1,
-                border: 0,
-            },
-            '&.Mui-disabled + .MuiSwitch-track': {
-                opacity: 0.5,
-            },
-            },
-            '&.Mui-focusVisible .MuiSwitch-thumb': {
-            color: '#33cf4d',
-            border: '6px solid #fff',
-            },
-            '&.Mui-disabled .MuiSwitch-thumb': {
-            color:
-                theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[600],
-            },
-            '&.Mui-disabled + .MuiSwitch-track': {
-            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
-            },
-        },
-        '& .MuiSwitch-thumb': {
-            boxSizing: 'border-box',
-            width: 22,
-            height: 22,
-        },
-        '& .MuiSwitch-track': {
-            borderRadius: 26 / 2,
-            backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
-            opacity: 1,
-            transition: theme.transitions.create(['background-color'], {
-            duration: 500,
-            }),
-        },
-    }));
-
 
     return (
         <div className={ styles.containerPage }>
@@ -187,10 +137,9 @@ export function CreateCarPage() {
                         </div>
                         <div className={styles.switchContainer}>
                             <p>Mercosul</p>
-                            <FormControlLabel
-                                control={<IOSSwitch sx={{ m: 1 }}
-                                defaultChecked={false} />}
-                                onChange={handleMercosulSwitchChange}
+                            <SwitchIOS
+                                checked={isMercosul}
+                                onChange={handleSwitchChange}
                             />
                         </div>
                     </div>
